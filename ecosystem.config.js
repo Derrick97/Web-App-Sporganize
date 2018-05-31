a13792f9f43c314b3da9a96db2770b74baa1ac1a
Joe Kaushal <jk1616@ic.ac.uk>
2018-05-31 22:24:16 +0100

    Attempt to auto-deploy via pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps: [
+    {
+      name: 'Sporganise',
+      script: 'Server.js',
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy: {
+    dev: {
+      user: 'www',
+      host: 'cloud-vm-45-111.doc.ic.ac.uk',
+      ref: 'origin/master',
+      repo: 'git@gitlab.doc.ic.ac.uk:g1727116/WebApps.git',
+      path: '/var/www/sporganise-dev',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev'
+      env: {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+}
