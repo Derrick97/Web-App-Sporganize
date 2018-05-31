b409656f916a998d29a3a977206aaebb640aacd8
Joe Kaushal <jk1616@ic.ac.uk>
2018-05-31 23:45:24 +0100

    Improve pm2 ecosystem config style

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,15 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps: [
+  'apps': [
     {
-      name: 'Sporganise',
-      script: 'Server.js',
-      env: {
-        NODE_ENV: 'dev'
+      'name'  : 'Sporganise',
+      'script': 'Server.js',
+      'env': {
+        'NODE_ENV': 'dev'
       },
-      env_production: {
-        NODE_ENV: 'production'
+      'env_production': {
+        'NODE_ENV': 'production'
       }
     }
   ],
@@ -20,15 +20,15 @@ module.exports = {
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy: {
-    dev: {
-      user: 'www',
-      host: 'cloud-vm-45-111.doc.ic.ac.uk',
-      ref : 'origin/master',
-      repo: 'git@gitlab.doc.ic.ac.uk:g1727116/WebApps.git',
-      path: '/var/www/sporganise-dev',
-      ssh_options: 'StrictHostKeyChecking=no',
-      post-deploy: 'npm install && pm2 reload ecosystem.config.js --env dev',
+  'deploy': {
+    'dev': {
+      'user': 'www',
+      'host': 'cloud-vm-45-111.doc.ic.ac.uk',
+      'ref' : 'origin/master',
+      'repo': 'git@gitlab.doc.ic.ac.uk:g1727116/WebApps.git',
+      'path': '/var/www/sporganise-dev',
+      'ssh_options': 'StrictHostKeyChecking=no',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env dev',
     }
   }
 }
