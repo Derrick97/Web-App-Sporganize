1aa828b2fcfb14944c322a946862ac94b7918a1c
Jian Rong <jr2216@matrix34.doc.ic.ac.uk>
2018-06-04 16:24:43 +0100

    Change layout of photospage.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 
-const { Pool } = require('pg')
+const {Pool} = require('pg')
 const pool = new Pool()
 
 const login_info = [
@@ -10,15 +10,15 @@ const login_info = [
         id: 1,
         email: 'jr2216@ic.ac.uk',
         pwd: '1234567',
-        eventsID: [1,2,3],
-        groupID: [21,22,23],
+        eventsID: [1, 2, 3],
+        groupID: [21, 22, 23],
     },
     {
         id: 2,
         email: 'cz1616@ic.ac.uk',
         pwd: 'zhangchi',
-        eventsID: [2,3],
-        groupID: [16,17],
+        eventsID: [2, 3],
+        groupID: [16, 17],
     }
 ];
 
@@ -64,6 +64,22 @@ const events = [
         date: '25rd May 10:00am',
         location: 'Imperial Sports Ground',
         status: 'Unknown'
+    },
+    {
+        id: 4,
+        name: 'After Exam fun!',
+        day: 'Monday',
+        date: '28nd May 9:00am',
+        location: 'Lee Abbey Sportsfield',
+        status: 'You Attended'
+    },
+    {
+        id: 5,
+        name: 'Leisure Football',
+        day: 'Monday',
+        date: '14nd June 9:00pm',
+        location: 'KCL Gym',
+        status: 'You Declined'
     }
 ]
 
@@ -97,9 +113,35 @@ const eventsupcoming = [
     }
 ]
 
-
-const eventsempty = [
+const groups = [
+    {
+        id: 16,
+        name: "IC Team",
+        eventsID: [1, 2],
+    },
+    {
+        id: 17,
+        name: "LSE Sports",
+        eventsID: [],
+    },
+    {
+        id: 21,
+        name: "KCL Team",
+        eventsID: [3],
+    },
+    {
+        id: 22,
+        name: "LSE Sports",
+        eventsID: [4, 5],
+    },
+    {
+        id: 23,
+        name: "Cambridge Sports",
+        eventsID: [],
+    }
 ]
+
+const eventsempty = []
 
 const events1 = [
     {
@@ -134,7 +176,7 @@ const events3 = [
     }
 ]
 
-function queryUser(email){
+function queryUser(email) {
     const user_info = login_info.filter((login) => {
         return login.email == email;
     })[0];
@@ -146,7 +188,7 @@ app.set('view engine', 'ejs');
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('Login')
@@ -159,9 +201,13 @@ app.get('/Login', (req, res) => {
 app.post('/login/:email', (req, res) => {
     const info = queryUser(req.params.email);
     if (info.pwd == req.body.pwd) {
-        if(info.eventsID.length>0){
+        if (info.eventsID.length > 0) {
             //Not all events, should be queried from DB.
-            res.render('EventsPage', {eventsprevious: eventsprevious, eventsupcoming: eventsupcoming, emailAdd: info.email});
+            res.render('EventsPage', {
+                eventsprevious: eventsprevious,
+                eventsupcoming: eventsupcoming,
+                emailAdd: info.email
+            });
         } else {
             res.render('MainPage');
         }
@@ -174,19 +220,19 @@ app.post('/login/:email', (req, res) => {
 app.get('/GroupChatList/:email/:teamID', (req, res) => {
     res.render('GroupChatList',
         {
-            groupID: [21,22,23],
+            groupID: [21, 22, 23],
             activeid: req.params.teamID,
             groupName: "Some Name Queried From DB.",
             emailAdd: req.params.email,
         });
 });
 
-app.get('/GroupChatList/:email',(req, res) =>{
-    const user = login_info.filter((user)=> {
+app.get('/GroupChatList/:email', (req, res) => {
+    const user = login_info.filter((user) => {
         return user.email == req.params.email;
     })[0];
     const groupID = user.groupID;
-    res.render('GroupChatList',{emailAdd:req.params.email, groupID: groupID, activeid:groupID[0]});
+    res.render('GroupChatList', {emailAdd: req.params.email, groupID: groupID, activeid: groupID[0]});
 })
 
 app.get('/Events/:email', async (req, res) => {
@@ -203,48 +249,69 @@ app.get('/Events/:email', async (req, res) => {
                 'status': ''
             }
         })
-    } catch(e) {
+    } catch (e) {
         res.status(500).send(e.stack)
         return
     }
 
-    res.render('EventsPage', {events: db_events, emailAdd:req.params.email,});
+    res.render('EventsPage', {events: db_events, emailAdd: req.params.email,});
 });
 
 
 app.get('/Events/:email/:teamname', (req, res) => {
     if (req.params.teamname == 'TeamA') {
-        res.render('EventsPage', {eventsprevious: events1, eventsupcoming: eventsempty, emailAdd:req.params.email,});
-    } else  if (req.params.teamname == 'TeamB') {
-        res.render('EventsPage', {eventsprevious: events2, eventsupcoming: eventsempty, emailAdd:req.params.email,});
-    } else  if (req.params.teamname == 'TeamC') {
-        res.render('EventsPage', {eventsprevious: eventsempty, eventsupcoming: events3, emailAdd:req.params.email,});
+        res.render('EventsPage', {eventsprevious: events1, eventsupcoming: eventsempty, emailAdd: req.params.email,});
+    } else if (req.params.teamname == 'TeamB') {
+        res.render('EventsPage', {eventsprevious: events2, eventsupcoming: eventsempty, emailAdd: req.params.email,});
+    } else if (req.params.teamname == 'TeamC') {
+        res.render('EventsPage', {eventsprevious: eventsempty, eventsupcoming: events3, emailAdd: req.params.email,});
     }
 });
 
 app.get('/Photos/:email', (req, res) => {
+    //Mock database query.
     const user = queryUser(req.params.email);
+    let eventsID = [];
+    for (let i = 0; i < user.groupID.length; i++) {
+        const group = groups.filter((group) => {
+            return group.id == user.groupID[i];
+        })[0];
+        eventsID.concat(group.eventsID);
+    }
+    let allEvents = [];
+    for (let i = 0; i < eventsID.length; i++) {
+        const event = events.filter((event) => {
+            return event.id == eventsID[i];
+        })[0];
+        allEvents.push(event);
+    }
+
     res.render('PhotosPage',
         {
-            emailAdd:req.params.email,
-            eventsID:user.eventsID,
-            activeID:-1,
+            groupsID: user.groupID,
+            emailAdd: req.params.email,
+            events: [events[0],events[1],events[2]],
+            activeID: -1,
         });
 });
 
-app.get('/Photos/:email/:eventID', (req, res) => {
+app.get('/Photos/:email/:groupID', (req, res) => {
     const user = queryUser(req.params.email);
+    const group = groups.filter((group) => {
+        return group.id == req.params.groupID;
+    });
+    //...Some query...
     res.render('PhotosPage',
         {
-            emailAdd:req.params.email,
-            eventID:req.params.eventID,
-            eventsID:user.eventsID,
-            activeID:req.params.eventID,
+            groupsID: user.groupID,
+            emailAdd: req.params.email,
+            events: [events[0]],
+            activeID: req.params.eventID,
         });
 });
 
 app.get('/Teams/:email', (req, res) => {
-    const user = login_info.filter((user)=> {
+    const user = login_info.filter((user) => {
         return user.email == req.params.email;
     })[0];
     const createGroupID = [21];
@@ -252,14 +319,15 @@ app.get('/Teams/:email', (req, res) => {
     //The two attribute above should be queried from database.
     res.render('TeamsPage', {
         events: events,
-        emailAdd:req.params.email,
+        emailAdd: req.params.email,
         createGroupID: createGroupID,
-        joinGroupID: joinGroupID,});
+        joinGroupID: joinGroupID,
+    });
 
 });
 
 app.post('/Teams/:email', (req, res) => {
-    const user = login_info.filter((user)=> {
+    const user = login_info.filter((user) => {
         return user.email == req.params.email;
     })[0];
     const createGroupID = [21];
@@ -271,26 +339,26 @@ app.post('/Teams/:email', (req, res) => {
         {
             createGroupID: createGroupID,
             joinGroupID: joinGroupID,
-            emailAdd:req.params.email,
-            events:events,
-            teamName:req.body.teamname,
-            teamType:req.body.teamtype,
+            emailAdd: req.params.email,
+            events: events,
+            teamName: req.body.teamname,
+            teamType: req.body.teamtype,
         })
 })
 
 app.get('/Settings/:email', (req, res) => {
-    res.render('SettingsPage',{emailAdd: req.params.email});
+    res.render('SettingsPage', {emailAdd: req.params.email});
 });
 
 app.get('/CreateTeam', (req, res) => {
     res.render('CreateTeam');
 })
 
-app.get('/ChangeStatus/:id',(req,res) =>{
-    const event = events.filter((event)=> {
+app.get('/ChangeStatus/:id', (req, res) => {
+    const event = events.filter((event) => {
         return event.id == req.params.id;
     })[0]
-    res.render('ChangeStatus',{
+    res.render('ChangeStatus', {
         eventID: event.id,
         eventName: event.name,
         eventStatus: event.status,
@@ -300,9 +368,10 @@ app.get('/ChangeStatus/:id',(req,res) =>{
     })
 });
 
-app.get('/Upload/:email',(req, res) => {
+app.get('/Upload/:email/:eventID', (req, res) => {
     res.render('UploadPhotos', {
         emailAdd: req.params.email,
+        eventID: req.params.eventID,
     })
 });
 
