ff6d9d59de5ac6cb56a778eefcfa418823675c9b
Jian Rong <jr2216@point39.doc.ic.ac.uk>
2018-06-09 23:56:18 +0100

    Elaborate Team details page, add alert for join team via code

--- database.js
+++ database.js
@@ -21,6 +21,13 @@ module.exports = {
         return users.rows[0]
     },
 
+    getCreatorForTeamID: async function(team_id) {
+        const query = ['SELECT * FROM sporganize.users_teams JOIN sporganize.users ON users.id = users_teams.user_id',
+            'WHERE users_teams.team_id = $1 AND users_teams.access_level = $2'].join(' ')
+        const creatorInfo = await pool.query(query, [team_id,'admin'])
+        return creatorInfo.rows[0]
+    },
+
     getUserForId: async function(id) {
         const query = 'SELECT * FROM sporganize.users WHERE id = $1'
         const users = await pool.query(query, [id])
@@ -40,12 +47,14 @@ module.exports = {
         return users.rows
     },
 
-    getAllUsersIDForTeam: async function(id){
-        const query = 'SELECT sporganize.users_teams.user_id FROM sporganize.users_teams WHERE team_id = $1'
+    getAllUsersInfoForTeam: async function(id){
+        const query = ['SELECT *',
+            'FROM sporganize.users_teams',
+            'JOIN sporganize.users ON users.id = users_teams.user_id',
+            'WHERE users_teams.team_id = $1'].join(' ')
         const users = await pool.query(query, [id])
         return users.rows
     },
-
 
     /* Team retrieval */
     getTeamForId: async function(id) {
@@ -108,6 +117,7 @@ module.exports = {
         return events
     },
 
+
     getAllEventsForUserIdWithAccessLevelAndStatus: async function(id) {
         const query = ['SELECT * ',
         'FROM sporganize.events JOIN sporganize.users_teams ON events.team_id = users_teams.team_id',
@@ -153,7 +163,7 @@ module.exports = {
         const teams = await pool.query(query, [code])
 
         const now = new Date()
-        for (const i = 0; i < teams.rows.length; i++) {
+        for (let i = 0; i < teams.rows.length; i++) {
             const expiry = new Date(teams.rows[i].expires)
             if (expiry > now) {
                 return teams.rows[i].team_id
@@ -161,6 +171,12 @@ module.exports = {
         }
 
         return -1
+    },
+
+    getCurrentJoinCodeForTeamID: async function(team_id){
+        const query = 'SELECT * FROM sporganize.join_codes WHERE join_codes.team_id = $1'
+        const join_code_info = await pool.query(query, [team_id])
+        return join_code_info.rows[0]
     },
 
     // Returns true on success, false on invalid code
@@ -219,7 +235,7 @@ module.exports = {
         await pool.query(query, [team_id, name, timestamp, duration, location])
         const allEvents = await this.getAllEventsForUserId(creator_user_id)
         const current_event_id = allEvents[allEvents.length-1].id
-        const all_users_for_current_event = await this.getAllUsersIDForTeam(team_id)
+        const all_users_for_current_event = await this.getAllUsersInfoForTeam(team_id)
         for(let i = 0; i<all_users_for_current_event.length; i++){
             await this.addUserToEventWithStatus(all_users_for_current_event[i].user_id, current_event_id, 'noreply')
         }
